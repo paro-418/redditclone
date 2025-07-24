480c59d6fa79741e3f9c8452109e916fddc3628d
paro-418 <vermapraween6543@gmail.com>
2025-07-24 14:44:09 +0530

    home screen complete

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,14 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import { StyleSheet, View, FlatList } from 'react-native';
+import React from 'react';
+import PostListItem from '../../components/PostListItem';
+import posts from '../../../assets/data/posts.json';
 const HomeScreen = () => {
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <FlatList
+        data={posts}
+        renderItem={({ item: post }) => <PostListItem post={post} />}
+        keyExtractor={({ id }) => id}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
