c8fbcc1b2bb967a7438b0e89058e15bf52911e0a
paro-418 <vermapraween6543@gmail.com>
2025-07-25 20:33:09 +0530

    added supabase client and dummy data in database

---
+++ src/lib/supabase.ts
@@ -0,0 +1,21 @@
+import { AppState } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseURL = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseURL, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true,
+  },
+});
+
+AppState.addEventListener('change', (state) => {
+  if (state === 'active') {
+    supabase.auth.startAutoRefresh();
+  } else supabase.auth.stopAutoRefresh();
+});
