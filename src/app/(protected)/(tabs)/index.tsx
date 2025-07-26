6273d8cfac08361e680e9b15e34677539e8200d2
paro-418 <vermapraween6543@gmail.com>
2025-07-26 22:23:28 +0530

    added typescript for supabase tables

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -1,8 +1,34 @@
 import { StyleSheet, View, FlatList } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PostListItem from '../../../components/PostListItem';
-import posts from '../../../../assets/data/posts.json';
+// import posts from '../../../../assets/data/posts.json';
+
+import { supabase } from '../../../lib/supabase';
+import { Tables } from '../../../types/database.types';
+
+export type PostWithGroupAndUsers = Tables<'posts'> & {
+  user: Tables<'users'>;
+  group: Tables<'groups'>;
+};
+
 const HomeScreen = () => {
+  const [posts, setPosts] = useState<PostWithGroupAndUsers[]>([]);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    const { data, error } = await supabase
+      .from('posts')
+      .select('*, group:groups(*), user:users!posts_user_id_fkey(*)');
+    console.log('error', error);
+    console.log('data', JSON.stringify(data));
+
+    if (error) {
+      console.log('error', error);
+    } else setPosts(data);
+  };
   return (
     <View>
       <FlatList
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,12 @@
 import { AppState } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import { Database } from '../types/database.types';
 
 const supabaseURL = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseURL, supabaseAnonKey, {
+export const supabase = createClient<Database>(supabaseURL, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
